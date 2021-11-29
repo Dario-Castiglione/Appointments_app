5b32f3b7fc7ec08431cd184fa66cc00287c4dff8
Dario Castiglione <dario.castiglione99@gmail.com>
2021-11-29 16:02:33 +0100

    .

--- js/index.js
+++ js/index.js
@@ -1,17 +1,29 @@
 //------------------------------------open
 const card = document.querySelectorAll(".card")
-const cardImg = document.querySelectorAll(".title-card")
+const titleCard = document.querySelectorAll(".title-card")
+const main = document.querySelector("main")
 
-const imgCard = [] 
-imgCard.push(...cardImg)
-imgCard.forEach(element =>{
+const cardDown = [] 
+cardDown.push(...titleCard)
+cardDown.forEach(element =>{
     element.addEventListener("click", (event)=>{
         let cardOpen = document.querySelector(".card-opened")
         if (cardOpen != null) cardOpen.classList.remove("card-opened")
         event.currentTarget.parentElement.classList.toggle("card-opened")
     })
 })
+/*
+main.addEventListener("click", (e)=>{
+    let isCompleted = "";
+    if (e.target.type==="checkbox") {
 
+        const element = tasks.find(element => element.id = e.target.id)
+        if (element.completed ==="true") isCompleted = "false"
+        else {element.completed === "true"}  isCompleted = "true"
+        console.log(tasks.find(element => element.id = e.target.id))
+    }
+})
+*/
 
 
 //------------------------------------------------------------GET PRODUCT
@@ -22,11 +34,11 @@ const render = (container, array) =>{
         const taskInput = document.createElement("input")
         taskP.innerText =`${element.title}`
         taskInput.setAttribute("type", "checkbox")
+        taskInput.setAttribute("id", element.id)
         if (element.completed === true) taskInput.setAttribute("checked",``)
         task.classList= "task"
         task.appendChild(taskP)
-        task.appendChild(taskInput)
-        
+        task.appendChild(taskInput)        
         container.appendChild(task)
     })
 
@@ -40,13 +52,17 @@ const filterData = (data) => {
     return notCompletedTasks , completedTasks
 }
 let tasks = []
+export let API = "https://jsonplaceholder.typicode.com/todos"
 const getProductList = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/todos")
+    const res = await fetch(API)
     const data = await res.json();
     filterData(data) 
-    
+    tasks = data
 }
+//console.log(data)
 getProductList()
+
+
 
 
 
