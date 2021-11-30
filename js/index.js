d6c73b52c67945f5adbde4c9460703424dd5ca92
Dario Castiglione <dario.castiglione99@gmail.com>
2021-11-30 11:35:44 +0100

    ..

--- js/index.js
+++ js/index.js
@@ -5,10 +5,10 @@ const main = document.querySelector("main")
 const wrapper = document.querySelectorAll(".wrapper")
 
 
-const cardDown = [] 
+const cardDown = []
 cardDown.push(...titleCard)
-cardDown.forEach(element =>{
-    element.addEventListener("click", (event)=>{
+cardDown.forEach(element => {
+    element.addEventListener("click", (event) => {
         let cardOpen = document.querySelector(".card-opened")
         if (cardOpen != null) cardOpen.classList.remove("card-opened")
         event.currentTarget.parentElement.classList.toggle("card-opened")
@@ -16,40 +16,30 @@ cardDown.forEach(element =>{
 })
 
 
-main.addEventListener("click", (e)=>{
-    let isCompleted = "";
-    if (e.target.type==="checkbox") {  
 
-        
+//-----------------------------------------------------------LOCAL STORAGE
+const taskino = JSON.parse(localStorage.getItem("taskini")) || "";
+console.log(taskino)
+//------------------------------------------check product
+main.addEventListener("click", (e) => {
+    let isCompleted = "";
+    if (e.target.type === "checkbox") {
     }
 })
-
-/*
-const newArray = array.map( (element) => {     
-   `<div class="task">
-    <p>${element.title}</p>
-    <input type="checkbox" id="${element.id}">
-    </div>`
- 
-})*/
-
 //------------------------------------------------------------GET PRODUCT
-const render = (container, array) =>{
-    function display(){
-        container.innerHTML= newArray
+const render = (container, array) => {
+    function display() {
+        container.innerHTML = newArray
     }
 
     const newArray = array
-    .map(
-      (element) =>   `<div class="task">
+        .map(
+            (element) => `<div class="task">
       <p>${element.title}</p>
       <input type="checkbox" ${element.completed ? "checked" : ""} id="${element.id}">
-      </div>` 
-      ).join("")
-      display(newArray)
-    
- 
-
+      </div>`
+        ).join("")
+    display(newArray)
 
 }
 const filterData = (data) => {
@@ -57,7 +47,7 @@ const filterData = (data) => {
     let completedTasks = data.filter(element => element.completed === true)
     render(wrapper[0], notCompletedTasks)
     render(wrapper[1], completedTasks)
-    return notCompletedTasks , completedTasks
+    return notCompletedTasks, completedTasks
 }
 
 let tasks = []
@@ -66,7 +56,7 @@ const getList = async () => {
     const res = await fetch(API)
     const data = await res.json();
     tasks = data
-    filterData(tasks) 
+    filterData(tasks)
     order(tasks)
 }
 getList()
@@ -75,84 +65,38 @@ getList()
 
 const form = document.querySelector(".add-form")
 
-form.addEventListener('submit', (event) =>{
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-
-    const newTask =  
-        {
-            "id": `${tasks.length + 1}`,
-            "title": `${form.firstElementChild.value}`,
-            "completed": false
-        }       
+    const newTask = {
+        "id": `${tasks.length + 1}`,
+        "title": `${form.firstElementChild.value}`,
+        "completed": false
+    }
     const addTask = () => {
-     tasks.push(newTask)
-     sorting(orderElement[0],tasks)
-     form.firstElementChild.value="";
-    }    
-    
+        tasks.push(newTask)
+        sorting(orderElement[0], tasks)
+        form.firstElementChild.value = "";
+    }
     addTask()
-    
 });
 //--------------------------------------order
-function orderByName(a, b) {
-    if (a.title < b.title) {
-        return -1
-    }
-    if (a.title > b.title) {
-        return 1;
-    }
-    return 0;
-}
-function orderById(a, b) {
-    if (a.id < b.id) {
-        return -1
-    }
-    if (a.id > b.id) {
-        return 1;
-    }
-    return 0;
-}
-function orderByIdRev(a, b) {
-    if (b.id < a.id) {
-        return -1
-    }
-    if (b.id > a.id) {
-        return 1;
-    }
-    return 0;
-}
-function sorting(orderElement,array){
-    switch (orderElement.value) {
-        case  "alfabetico":
-            array.sort(orderByName)
-                break;
-        case  "dataC":
-            array.sort(orderById)
-                break;        
-        case  "dataD":
-            array.sort(orderByIdRev)
-                break;        
-            default:
-                break;
-        }
-        filterData(tasks)
-}
+
+import { sorting } from "./sorting.js";
 const orderElement = document.querySelectorAll("select")
-function order(array){
-      
+
+function order(array) {
     orderElement.forEach(select => select.addEventListener('change', (e) => {
-        console.log("ciao")
-        sorting(select,array)
+        sorting(select, array)
     }))
 }
 
 //--------------------------------------resize
 
-let height = window.innerHeight /100 * 90;
-main.style.height=`${height}px`
+let height = window.innerHeight / 100 * 90;
+main.style.height = `${height}px`
 window.addEventListener('resize', () => {
-    height = window.innerHeight /100 * 90;
-    main.style.height=`${height}px`
-  });
+    height = window.innerHeight / 100 * 90;
+    main.style.height = `${height}px`
+});
 
-
+export { filterData, tasks }
---
+++ js/sorting.js
@@ -0,0 +1,45 @@
+import {filterData, tasks} from "./index.js"
+
+function orderByName(a, b) {
+    if (a.title < b.title) {
+        return -1
+    }
+    if (a.title > b.title) {
+        return 1;
+    }
+    return 0;
+}
+function orderById(a, b) {
+    if (a.id < b.id) {
+        return -1
+    }
+    if (a.id > b.id) {
+        return 1;
+    }
+    return 0;
+}
+function orderByIdRev(a, b) {
+    if (b.id < a.id) {
+        return -1
+    }
+    if (b.id > a.id) {
+        return 1;
+    }
+    return 0;
+}
+export function sorting(orderElement,array){
+    switch (orderElement.value) {
+        case  "alfabetico":
+            array.sort(orderByName)
+                break;
+        case  "dataC":
+            array.sort(orderById)
+                break;        
+        case  "dataD":
+            array.sort(orderByIdRev)
+                break;        
+            default:
+                break;
+        }
+        filterData(tasks)
+}
